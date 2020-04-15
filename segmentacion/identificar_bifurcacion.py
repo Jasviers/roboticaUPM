8b8d3e226f779f2cde1e1f0104014208e8119bd5
Marcos-TS <61843907+Marcos-TS@users.noreply.github.com>
2020-04-15 18:44:06 +0200

    Reconocimiento del camino

--- segmentacion/identificar_bifurcacion.py
+++ segmentacion/identificar_bifurcacion.py
@@ -5,58 +5,147 @@ import numpy as np
 import math
 
 def existen_bifurcaciones(img,imgMrk, centro):
-   imNp = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+ 
+   linImg = (imgMrk==1).astype(np.uint8)*255
+   _,contours, _ = cv2.findContours(linImg, cv2.RETR_LIST, cv2.CHAIN_APPROX_NONE)#sacamos los contornos de la linea
+   bordes=[]
+   for cont in contours:#aqui vamos a guardar las distintas salidas y el borde de la imagen por el que salen
+      bordeN=[]
+      bordeS=[]
+      bordeW=[]
+      bordeE=[]
+      for point in cont:
+         height, width = img.shape[:2]
+         if (point[0][0]==0):  bordeW.append(tuple(point[0]))
+         elif (point[0][0]==width-1):   bordeE.append(tuple(point[0]))
+         elif (point[0][1]==0):   bordeN.append(tuple(point[0]))
+         elif (point[0][1]==height-1):   bordeS.append(tuple(point[0]))
+      bordeN.sort()
+      bordeS.sort()
+      bordeW.sort()
+      bordeE.sort()
+      #el siguiente codigo se encarga de poder separar los puntos de cada borde en distintos bordes
+      if len(bordeW)>0:
+         definitiva=[]
+         auxiliar=[]
+         for point in bordeW:
+            if len(auxiliar)<1:
+               auxiliar.append(point)
+            elif math.sqrt(pow((point[0]-auxiliar[-1][0]),2)+pow((point[1]-auxiliar[-1][1]),2))<5:
+               auxiliar.append(point)
+            else:
+               definitiva.append(auxiliar)
+               auxiliar=[]
+               auxiliar.append(point)
+         definitiva.append(auxiliar)
+         for lista in definitiva:
+            bordes.append(lista)
 
-   imgMrk = np.reshape(imgMrk, (img.shape[0], img.shape[1]))
-   paleta = np.array([[0,0,255], [255,0,0], [0,255,0]], dtype=np.uint8)
-   imgDest =  cv2.medianBlur(cv2.cvtColor(paleta[imgMrk], cv2.COLOR_RGB2BGR), 5)
-   imgDest[np.where((imgDest==[0,0,255]).all(axis=2))] = [0,255,0]
-   gray = cv2.cvtColor(imgDest, cv2.COLOR_BGR2GRAY) 
-   edged = cv2.Canny(gray, 30, 200) 
-   img2, contours, hierarchy = cv2.findContours(edged, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE) 
+      if len(bordeE)>0:
+         definitiva=[]
+         auxiliar=[]
+         for point in bordeE:
+            if len(auxiliar)<1:
+               auxiliar.append(point)
+            elif math.sqrt(pow((point[0]-auxiliar[-1][0]),2)+pow((point[1]-auxiliar[-1][1]),2))<5:
+               auxiliar.append(point)
+            else:
+               definitiva.append(auxiliar)
+               auxiliar=[]
+               auxiliar.append(point)
+         definitiva.append(auxiliar)
+         for lista in definitiva:
+            bordes.append(lista)
+
+      if len(bordeN)>0:
+         definitiva=[]
+         auxiliar=[]
+         for point in bordeN:
+            if len(auxiliar)<1:
+               auxiliar.append(point)
+            elif math.sqrt(pow((point[0]-auxiliar[-1][0]),2)+pow((point[1]-auxiliar[-1][1]),2))<5:
+               auxiliar.append(point)
+            else:
+               definitiva.append(auxiliar)
+               auxiliar=[]
+               auxiliar.append(point)
+         definitiva.append(auxiliar)
+         for lista in definitiva:
+            bordes.append(lista)
+
+      if len(bordeS)>0:
+         definitiva=[]
+         auxiliar=[]
+         for point in bordeS:
+            if len(auxiliar)<1:
+               auxiliar.append(point)
+            elif math.sqrt(pow((point[0]-auxiliar[-1][0]),2)+pow((point[1]-auxiliar[-1][1]),2))<5:
+               auxiliar.append(point)
+            else:
+               definitiva.append(auxiliar)
+               auxiliar=[]
+               auxiliar.append(point)
+         definitiva.append(auxiliar)
+         for lista in definitiva:
+            bordes.append(lista)
    
    camino = []
    result = 0
    salidas = []
    auxiliar = []
    nuevo_centro =()
+   puntos_salida=[]
+   contornolinea=[]
 
-   if not any(centro):
-      for i in range(len(contours)):
-         cont = contours[i]
+   if not any(centro):#si no existe un centro previo, buscamos uno que interseccione con el borde inferior de la imagen
+      for i in range(len(bordes)):
+         cont = bordes[i]
          for e in range(len(cont)):
             point = cont[e]
             height, width = img.shape[:2]
-            if (point[0][1]==height-1):
-               auxiliar.append(point[0])
-               camino.append(cont)
-            elif (point[0][0]==0) or (point[0][0]==width-1) or (point[0][1]==0) or (point[0][1]==height-1):
-               salidas.append(tuple(point[0]))
-      x = abs(auxiliar[0][0]-auxiliar[1][0])/2
-      nuevo_centro = min(auxiliar[0][0],auxiliar[1][0])+x,auxiliar[0][1]
+            if cont not in auxiliar and (point[1]==height-1):
+               auxiliar.append(cont)
+            elif cont not in salidas:
+               salidas.append(cont)
+      if len(auxiliar)>0:
+         x = auxiliar[0]
+         x = x[len(x)/2]#el centro estara en la posicion intermedia del borde de entrada
+         nuevo_centro = x[0],x[1]
+      if len(salidas)>0:
+         for salida in salidas:
+            x = salida[len(salida)/2]
+            puntos_salida.append(x)
 
    else:
-      for i in range(len(contours)):
-         cont = contours[i]
+      for i in range(len(bordes)):#si existe un centro previo
+         cont = bordes[i]
          for e in range(len(cont)):
             point = cont[e]
             height, width = img.shape[:2]
-            if (point[0][1]==height-1) and ((math.sqrt(pow((point[0][0]-centro[0]),2)+pow((point[0][1]-centro[1]),2)))<30):
-               auxiliar.append(point[0])
-               camino.append(cont)
-            elif (point[0][0]==0) or (point[0][0]==width-1) or (point[0][1]==0) or (point[0][1]==height-1):
-               salidas.append(tuple(point[0]))
-      x = abs(auxiliar[0][0]-auxiliar[1][0])/2
-      nuevo_centro = min(auxiliar[0][0],auxiliar[1][0])+x,auxiliar[0][1]
+            if cont not in auxiliar and ((math.sqrt(pow((point[0]-centro[0]),2)+pow((point[1]-centro[1]),2)))<20):
+               auxiliar.append(cont)#el nuevo centro deberia estar cerca del antiguo
+            elif cont not in salidas:
+               salidas.append(cont)
+      if len(auxiliar)>0:
+         x = auxiliar[0]
+         x = x[len(x)/2]#el centro estara en la posicion intermedia del borde de entrada
+         nuevo_centro = x[0],x[1]
+      else: nuevo_centro=(160,170) #aqui habria que hacer un tratamiento para cuando perdamos la linea
+      if len(salidas)>0:
+         for salida in salidas:
+            x = salida[len(salida)/2]
+            puntos_salida.append(x)#cada salida es el punto medio de los bordes de salida
 
-   if len(camino)>1:
-      for i in range(len(camino[0])):
-         point = camino[0][i]
-         if (abs(cv2.pointPolygonTest(camino[1], tuple(point[0]), measureDist = True))>40):
-            result = 1
-      for i in range(len(camino[1])):
-         point = camino[1][i]
-         if (abs(cv2.pointPolygonTest(camino[0], tuple(point[0]), measureDist = True))>40):
-            result = 1
-   return result, salidas, nuevo_centro
+   for cont in contours:#identificamos el contorno que determina nuestro camino
+      if cv2.pointPolygonTest(cont, nuevo_centro, False)== 0.0:
+         contornolinea=cont
+
+   for x in puntos_salida:#sacamos el punto de entrada de la lista de salidas
+      if type(x)==tuple:
+         if x==nuevo_centro:  puntos_salida.remove(x)
+   #elimino las salidas que no forman parte del camino
+   puntos_salida[:] = [x for x in puntos_salida if len(contornolinea)>0 and abs(cv2.pointPolygonTest(contornolinea, x, True))<5]
+
+   #devuelvo contorno del camino, salidas y centro
+   return contornolinea, puntos_salida, nuevo_centro
 
