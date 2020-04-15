092eee680cf1cb6a43612b162cede4f5a2435ebc
Marcos-TS <61843907+Marcos-TS@users.noreply.github.com>
2020-04-15 09:22:53 +0200

    Calculo inicial del camino

--- segmentacion/identificar_bifurcacion.py
+++ segmentacion/identificar_bifurcacion.py
@@ -2,8 +2,9 @@ import cv2
 from scipy.misc import imread, imsave
 from matplotlib import pyplot as plt
 import numpy as np
+import math
 
-def existen_bifurcaciones(img,imgMrk):
+def existen_bifurcaciones(img,imgMrk, centro):
    imNp = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
    imgMrk = np.reshape(imgMrk, (img.shape[0], img.shape[1]))
@@ -15,19 +16,47 @@ def existen_bifurcaciones(img,imgMrk):
    img2, contours, hierarchy = cv2.findContours(edged, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE) 
    
    camino = []
-   result = False
+   result = 0
+   salidas = []
+   auxiliar = []
+   nuevo_centro =()
 
-   for i in range(len(contours)):
-      cont = contours[i]
-      for e in range(len(cont)):
-         point = cont[e]
-         height, width = img.shape[:2]
-         if (point[0][1]==height-1):
-            camino.append(cont)
+   if not any(centro):
+      for i in range(len(contours)):
+         cont = contours[i]
+         for e in range(len(cont)):
+            point = cont[e]
+            height, width = img.shape[:2]
+            if (point[0][1]==height-1):
+               auxiliar.append(point[0])
+               camino.append(cont)
+            elif (point[0][0]==0) or (point[0][0]==width-1) or (point[0][1]==0) or (point[0][1]==height-1):
+               salidas.append(tuple(point[0]))
+      x = abs(auxiliar[0][0]-auxiliar[1][0])/2
+      nuevo_centro = min(auxiliar[0][0],auxiliar[1][0])+x,auxiliar[0][1]
 
-   for i in range(len(camino[0])):
-      point = camino[0][i]
-      if (abs(cv2.pointPolygonTest(camino[1], tuple(point[0]), measureDist = True))>50):
-         result = True
-   return result
+   else:
+      for i in range(len(contours)):
+         cont = contours[i]
+         for e in range(len(cont)):
+            point = cont[e]
+            height, width = img.shape[:2]
+            if (point[0][1]==height-1) and ((math.sqrt(pow((point[0][0]-centro[0]),2)+pow((point[0][1]-centro[1]),2)))<30):
+               auxiliar.append(point[0])
+               camino.append(cont)
+            elif (point[0][0]==0) or (point[0][0]==width-1) or (point[0][1]==0) or (point[0][1]==height-1):
+               salidas.append(tuple(point[0]))
+      x = abs(auxiliar[0][0]-auxiliar[1][0])/2
+      nuevo_centro = min(auxiliar[0][0],auxiliar[1][0])+x,auxiliar[0][1]
+
+   if len(camino)>1:
+      for i in range(len(camino[0])):
+         point = camino[0][i]
+         if (abs(cv2.pointPolygonTest(camino[1], tuple(point[0]), measureDist = True))>40):
+            result = 1
+      for i in range(len(camino[1])):
+         point = camino[1][i]
+         if (abs(cv2.pointPolygonTest(camino[0], tuple(point[0]), measureDist = True))>40):
+            result = 1
+   return result, salidas, nuevo_centro
 
